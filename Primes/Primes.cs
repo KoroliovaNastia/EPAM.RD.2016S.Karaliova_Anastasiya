f12b8c23670822f98cf6818658580c67fdc7a4fc
Anastasiya_Karaliova <Anastasiya_Karaliova@EPBYMINW1945>
2016-07-12 16:38:42 +0300

    Add project files.

---
+++ Primes/Primes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primes
+{
+    public class Primes
+    {
+        public  bool IsPrime(int number)
+        {
+            if (number == 0 || number==2 || number % 2 == 0)
+                return false;
+            for(int i=3; i<number; i+=2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public IEnumerable<int> GetEnumerator(int limit)
+        {
+            if (limit <= 0)
+                throw new ArgumentException();
+            for (int i = 0; i <= limit; i++)
+            {
+                if (IsPrime(i))
+                    yield return i;
+                
+            }
+            
+        }
+    }
+}
---
+++ PrimesTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Primes;
+
+namespace PrimesTest
+{
+    [TestClass]
+    public class PrimesTest
+    {
+        [TestMethod]
+        public void GetEnumerator_IsNull_ReturnMinusOne()
+        {
+            Primes.Primes list = new Primes.Primes();
+            var result = 
+            Assert.IsNull(result);
+        }
+    }
+}
