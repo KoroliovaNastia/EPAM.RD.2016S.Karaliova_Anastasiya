08993f90964e34faa6f2ea6b005633efad7fe9e1
KoroliovaNastia <K.Nastia.san@mail.ru>
2016-07-14 14:28:42 +0300

    tests

--- Day1/StorageSystem/DAL/Entities/User.cs
+++ Day1/StorageSystem/DAL/Entities/User.cs
@@ -13,7 +13,38 @@ namespace DAL.Entities
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
-        public Records VisaRecords { get; set; }
+        public IEnumerable<Records> VisaRecords { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, null)) return false;
+            var user = obj as User;
+            if (user == null)
+                return false;
+            return Equals(user);
+        }
+
+        public bool Equals(User other)
+        {
+            if ((String.CompareOrdinal(FirstName, other.FirstName) == 0) &&
+                (String.CompareOrdinal(LastName, other.LastName) == 0)
+                && (DateOfBirth == other.DateOfBirth) && (VisaRecords.SequenceEqual(other.VisaRecords)) &&
+                (Gender == other.Gender))
+                return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hash = (FirstName != null ? FirstName.GetHashCode() : 777) +
+                           (LastName != null ? LastName.GetHashCode() : 777);
+                hash = hash*(DateOfBirth.DayOfYear ^ 777)+Gender.GetHashCode();
+            return hash;
+            }
+            
+        }
     }
 
     public enum Gender
---
+++ Day1/StorageSystem/DALTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL.Entities;
+using DAL.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace DALTest
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        [TestMethod]
+        public void AddUser_AddThreeUsers_ReturnArrayWithThreeUsers()
+        {
+            UserService service = new UserService();
+            User user1 = new User {FirstName = "Lisa", LastName = "Rich"};
+            User user2 = new User {FirstName = "Nick", LastName = "Name"};
+            User user3 = new User {FirstName = "Tim", LastName = "Rodny"};
+
+            var result = new List<int> {service.AddUser(user1), service.AddUser(user2), service.AddUser(user3)};
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(service.Users);
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(3, service.Users.Count);
+        }
+
+        [TestMethod]
+        public void AddUser_AddAnExistUser_ReturnWithUnchangetCount()
+        {
+            UserService service = new UserService();
+            User user1 = new User { FirstName = "Lisa", LastName = "Rich" };
+            User user2 = new User { FirstName = "Lisa", LastName = "Rich" };
+            List<int> result = new List<int> { service.AddUser(user1), service.AddUser(user2)};
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(service.Users);
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(1, service.Users.Count);
+        }
+    }
+}
