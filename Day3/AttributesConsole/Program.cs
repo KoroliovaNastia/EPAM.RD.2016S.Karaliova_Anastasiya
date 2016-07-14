c6a8f80776533336ffdda625c88b4280a3f401ab
KoroliovaNastia <K.Nastia.san@mail.ru>
2016-07-15 01:28:29 +0300

    day3-console

---
+++ Day3/AttributesConsole/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Attributes;
+
+namespace AttributesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Type userType = typeof(User);
+           // InstantiateUserAttribute[] instantiateUserAttribute =
+           //     (InstantiateUserAttribute[])Attribute.GetCustomAttributes(userType, typeof(InstantiateUserAttribute));
+           // MatchParameterWithPropertyAttribute[] matchParameterWithPropertyAttribute =
+           //     (MatchParameterWithPropertyAttribute[])
+           //         Attribute.GetCustomAttributes(userType, typeof(MatchParameterWithPropertyAttribute));
+           // //PropertyInfo[] prop = userType.GetProperties();
+           //Console.WriteLine(instantiateUserAttribute[0].FirsName);
+           //Console.WriteLine(instantiateUserAttribute[1].FirsName);
+           //Console.WriteLine(instantiateUserAttribute[2].FirsName);
+           // List<User> users = new List<User>();
+
+           // PropertyInfo[] props = typeof(User).GetProperties();
+           // foreach (PropertyInfo prop in props)
+           // {
+           //     object[] attrs = prop.GetCustomAttributes(true);
+           //     foreach (object attr in attrs)
+           //     {
+           //         InstantiateUserAttribute userAttr = attr as InstantiateUserAttribute;
+           //         if (userAttr != null)
+           //         {
+           //             //string propName = prop.P;
+           //             //string firstName = userAttr.FirsName;
+           //             //string lastName = userAttr.LastName;
+           //             //int id = userAttr.id;
+           //             users.Add(new User(userAttr.id) { FirstName = userAttr.FirsName, LastName = userAttr.LastName });
+           //         }
+           //     }
+           // }
+           // foreach (var user in users)
+           // {
+           //     Console.WriteLine("User ID:" + user.Id);
+           //     Console.WriteLine("First name:"+user.FirstName);
+           //     Console.WriteLine("Last name:" + user.LastName);
+           //     Console.WriteLine("-----------------------------");
+           // }
+
+            Type type = typeof(InstantiateUserAttribute);
+
+            //iterating through the attribtues of the Rectangle class
+            foreach (Object attributes in type.GetCustomAttributes(false))
+            {
+                InstantiateUserAttribute dbi = (InstantiateUserAttribute)attributes;
+                if (null != dbi)
+                {
+                    Console.WriteLine("ID: {0}", dbi.id);
+                    Console.WriteLine("First Name: {0}", dbi.FirsName);
+                    Console.WriteLine("Last Name: {0}", dbi.LastName);
+                    Console.WriteLine("-----------------------------");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day3/AttributesConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("AttributesConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AttributesConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d01b6753-f9f4-4d83-9b4d-b1f4c232a00c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
