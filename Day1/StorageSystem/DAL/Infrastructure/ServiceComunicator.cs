2af74f7c4f870a0d228ce01a19128c28ce675998
KoroliovaNastia <K.Nastia.san@mail.ru>
2016-08-01 23:46:38 +0300

    fix

---
+++ Day1/StorageSystem/DAL/Infrastructure/ServiceComunicator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Infrastructure
+{
+    [Serializable]
+    public class ServiceComunicator
+    {
+        public event EventHandler<ActionEventArgs> Message;
+        //private Sender<BllUser> _sender;
+        public string sendMessage;
+        public void Send(ActionEventArgs args)
+        {
+            sendMessage = args.Message;
+        }
+
+        public string GetMessage()
+        {
+            if (Message == null)
+                return "no action";
+            return sendMessage;
+        }
+    }
+}
